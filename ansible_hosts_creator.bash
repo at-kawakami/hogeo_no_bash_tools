423400b05c6ee8bbda414c77c663e2c59cc67378
at-kawakami <centos@ip-172-31-45-0.ap-northeast-1.compute.internal>
2019-10-04 09:19:03 +0000

    mod ansible_hosts_creator.bash

--- ansible_hosts_creator.bash
+++ ansible_hosts_creator.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
-IFS=
-target_services=(web adm api batch)
+IFS=$'\n'
+target_services=$1
+if [ -z "$target_services" ];then
+  target_services=(web adm api)
+fi
+
 for service in ${target_services[@]}; do
   echo ["$service"]
-  grep $service /etc/hosts|awk '{print $2,"ansible_ssh_host="$1 " ansible_python=/usr/bin/python3 ansible_ssh_user=ubuntu"}'
+  targets=`grep $service /etc/hosts|awk '{print $2,"ansible_ssh_host="$1 " ansible_python=/usr/bin/python3 db_migrate=True ansible_ssh_user=ubuntu"}'`
+  count=1
+  for i in ${targets[@]};do
+    if [ $count -ne 1 ];then
+      echo $i|sed -e "s/db_migrate=True/db_migrate=False/g"
+    else
+      echo $i
+    fi
+    count=$(( count + 1 ))
+  done
 done
